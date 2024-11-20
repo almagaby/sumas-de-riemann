baa8fe58e774a1d68f19026f9299513f3c37d912
almagaby <82476125+almagaby@users.noreply.github.com>
2024-11-19 22:02:53 -0600

    Add files via upload

---
+++ ejercicio2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	
+	double a= 300;
+	double b = 4;
+	double res = a/ b;
+	
+	printf("El resultado es =%f", res);
+	
+	printf("\n");
+	system("Pause");
+    return 0;
+	
+	}
